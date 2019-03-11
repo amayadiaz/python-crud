77a49638e94a459665faf0a8cbb249df68af0898
amayadiaz <julian.amayad@gmail.com>
2019-03-10 23:38:48 -0500

    Add search client and cycles

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 
+import sys 
+
 clients = 'Lionel,Gerard,'
 
 def create_client(client_name):
@@ -20,6 +22,17 @@ def update_client(client_name):
     else: 
         not_in_list_msg
 
+def search_client(client_name):
+    global clients
+
+    clients_list = clients.split(',')
+    for client in clients_list:
+        if client_name != client:
+            continue
+        else:
+            return True
+
+
 def delete_client(client_name):
     global clients
 
@@ -30,10 +43,20 @@ def delete_client(client_name):
 
 
 def get_client_name():
-    return input('What is the client name ? ')
+    client_name = None
+
+    while not client_name: 
+        client_name = input('What is the client name ? ')
+
+        if client_name == 'exit': 
+            client_name = None
+            break 
+
+    if not client_name: 
+        sys.exit()
 
 def not_in_list_msg():
-    print('Client is not in client\'s list')
+    print('Name is not in client\'s list')
     
 
 
@@ -48,12 +71,13 @@ def add_comma():
 
 
 def print_welcome():
-    print('WELCOME TO PLATZI VENTAS')
+    print('WELCOME PYTHON CRUD')
     print('*' * 50)
     print('What would you like to do today ?')
     print('[C]reate client')
     print('[U]pdate client')
     print('[D]elete client')
+    print('[S]earch client')
 
 
 print_welcome()
@@ -72,6 +96,13 @@ elif command == 'U':
 elif command == 'D':
     client_name = get_client_name()
     delete_client(client_name)
-    list_clients()    
+    list_clients()  
+elif command == 'S':
+    client_name = get_client_name()
+    found = search_client(client_name)
+    if found: 
+        print('The client is in the client\'s list')
+    else:
+        print('The name {} is not in our client\'s list'.format(client_name))
 else: 
     print('Invalid command')
